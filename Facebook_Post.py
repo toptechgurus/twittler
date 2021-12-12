e5621454f79daedf534ba06f28a8f16b650abf47
atirumanur <atirumanur@ironsidegroup.com>
2021-12-11 22:19:15 -0500

    merged Kalais and Sunthars changes

--- Facebook_Post.py
+++ Facebook_Post.py
@@ -5,15 +5,15 @@ import openpyxl
 
 from selenium.webdriver.support.select import Select
 
-driver = webdriver.Chrome(executable_path=r"C:\Users\91787\PycharmProjects\selenium\driver\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=r"C:\Users\atirumanur\Documents\chromedriver.exe")
 driver.get(r"https://www.facebook.com/?stype=lo&jlou=AfefAFF_CGmgmOKtvoxd"
            r"-VftIBiMTQ7Yt1B0o4mPyqkMUcoPTf2__9y0vSJBaMR1aGoQlzm62x-v5NXY4zrPA19F685_HESvmg2dS2JDndABvQ&smuh=3467&lh"
            r"=Ac8QmNCITNDEE_bRRd8")
 driver.maximize_window()
 driver.implicitly_wait(5)
-emailid = 'ttg.test3@gmail.com'
+emailid = 'ttg-test1@protonmail.com'
 passw = 'thisisatesT#22'
-post = 'test post'
+post = 'test post # 3'
 email = driver.find_element_by_xpath('//*[@id="email"]')
 email.click()
 email.send_keys(emailid)
